d3e4919db9e7bf587893634e466ea809462afb8c
ValentinaSimonova20 <simonova.valentina19@gmail.com>
2020-12-05 17:48:50 +0300

    Добавление страницы настроек

---
+++ app/src/main/java/com/startandroid/trashstatf/SettingsFragment.java
@@ -0,0 +1,36 @@
+package com.startandroid.trashstatf;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class SettingsFragment extends Fragment {
+
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        getActivity().setTitle("Настройки");
+        setHasOptionsMenu(true);
+
+        return inflater.inflate(R.layout.fragment_advice,container,false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+
+
+    }
+}
